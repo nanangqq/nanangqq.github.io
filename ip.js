edb61b1fccc31d2996def7fa882bdc937361ea85
nanangqq <gingggg@gmail.com>
2020-01-26 13:53:56 +0900

    getting hostname

--- ip.js
+++ ip.js
@@ -1,18 +1,34 @@
 const http = require('http');
-const hostname = 'localhost';
-let port;
-if (process.argv.length === 2) {
-port = 3000;
-} else if (Number(process.argv[2])<9999) {
-port = Math.floor(Number(process.argv[2]));
-}
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/plain');
-console.log(req.connection.remoteAddress);
-res.end(req.connection.remoteAddress);
+const exec = require('child_process').exec;
+
+let hostname;
+exec('hostname -i', (error, stdout, stderr)=>{
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    hostname = stdout;
+
+    if (error !== null) {
+        console.log('exec error: ' + error);
+        hostname = 'localhost'
+    }
+
+    let port;
+    if (process.argv.length === 2) {
+        port = 3000;
+    } else if (Number(process.argv[2])<9999) {
+        port = Math.floor(Number(process.argv[2]));
+    }
+
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        console.log(req.connection.remoteAddress);
+        res.end(req.connection.remoteAddress);
+    });
+
+    server.listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:${port}/`);
+    });
+
 });
-server.listen(port, hostname, () => {
-//console.log(process.argv);
-console.log(`Server running at http://${hostname}:${port}/`);
-});
+
